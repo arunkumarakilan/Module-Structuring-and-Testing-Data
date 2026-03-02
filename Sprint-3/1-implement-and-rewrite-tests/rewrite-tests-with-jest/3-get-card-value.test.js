dc065a92116cfb657181f4dec9f17cba44e01eeb
Arunkumar Akilan <arunkumarakilanarun@gmail.com>
2026-03-02 21:08:05 +0000

    Implement and test getCardValue for all valid and invalid inputs

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -8,7 +8,27 @@ const getCardValue = require("../implement/3-get-card-value");
 test(`Should return 11 when given an ace card`, () => {
   expect(getCardValue("A♠")).toEqual(11);
 });
-
+test(`should return 9 when given a 9♠ card`,()=>{
+  expect(getCardValue("9♠")).toEqual(9);
+});
+test (`should return 10 when given a face card K♦`, ()=>{
+  expect(getCardValue("K♦")).toEqual(10);
+});
+test(`should return 10 when given a 10`, ()=>{
+  expect(getCardValue("10♥")).toEqual(10);
+});
+test(`should throw error for invalid Ranks`, ()=>{
+  expect(()=> getCardValue("11♠")).toThrow();
+});
+test(`should throw error for invalid Suits`, ()=>{
+  expect(()=> getCardValue("A?")).toThrow();
+});
+test(`should throw error for missing Suits`,()=>{
+  expect(()=> getCardValue("A")).toThrow();
+});
+test(`should throw error for non String input`, ()=>{
+  expect(()=> getCardValue("10")).toThrow();
+});
 // Suggestion: Group the remaining test data into these categories:
 //   Number Cards (2-10)
 //   Face Cards (J, Q, K)
