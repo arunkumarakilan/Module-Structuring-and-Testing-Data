b691c83ad51a0b0fa2502d4daca983e6e8950fd0
Arunkumar Akilan <arunkumarakilanarun@gmail.com>
2026-03-04 11:54:48 +0000

    Add comment to explain my code

--- Sprint-3/4-stretch/card-validator.js
+++ Sprint-3/4-stretch/card-validator.js
@@ -1,17 +1,19 @@
 function cardValidator(cardNumber)
 {
+    //check the card length must be 16 digit if the card number is less than 16 this condition will throw error
     if(cardNumber.length !==16)
     {
         throw new Error("card number length must be 16");
     }
     let sum = 0;
+    // Loop through each digit to check if it is a valid number
     for(let i = 0; i<15; i++)
-    {
+    {  // if character is not between "0" to "9", it's not a digit.
         if(cardNumber[i]<"0" || cardNumber[i]>"9")
         {
             throw new Error("not a valid number");
         }
-       
+    //convert the digit to number and add it to sum total.
         sum = sum + Number(cardNumber[i]);
     }
     for(let i = 1; i< cardNumber.length;i++)
@@ -21,6 +23,7 @@ function cardValidator(cardNumber)
           throw new Error(" should have at least two different digits represented");
         }
     }
+    // This condition will check the sum of all digit must be greater than 16 if its less than or Equal to 16 this condition will throw error
     if(sum <= 16)
     {
         throw new Error("sum of all number less than 16");
